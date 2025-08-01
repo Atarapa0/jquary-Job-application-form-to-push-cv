cb185624852daf1fe72629b277b1603f73821658
Atarapa0 <furkaan.er.34@gmail.com>
2025-08-01 20:34:36 +0300

    Add Inter font styles to varieble.css with multiple weights and styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,7 +64,7 @@ $(document).ready(function () {
         required: true,
         digits: true,  //pattern: /^[0-9]{10,11}$/,
         minlength: 10,
-        maxlength: 11
+        maxlength: 13
       },
       workplaceType: {
         required: true
@@ -76,31 +76,6 @@ $(document).ready(function () {
         required: true
       },
 
-    },
-    messages: {
-      name: {
-        required: "Ad soyad zorunludur",
-        minlength: "En az 2 karakter olmalı"
-      },
-      email: {
-        required: "Email zorunludur",
-        email: "Geçerli bir email adresi girin"
-      },
-      phone: {
-        required: "Telefon zorunludur",
-        digits: "Sadece rakam girin",
-        minlength: "10 haneli olmalı",
-        maxlength: "11 haneli olmalı"
-      },
-      workplaceType: {
-        required: "Çalışma türü seçilmelidir"
-      },
-      employmentType: {
-        required: "İstihdam türü seçilmelidir"
-      },
-      jobLocation: {
-        required: "Lokasyon seçilmelidir"
-      }
     },
     errorPlacement: function (error, element) {
       if (element.attr("name") === "workplaceType" || element.attr("name") === "employmentType" || element.attr("name") === "jobLocation") {
@@ -116,6 +91,8 @@ $(document).ready(function () {
       return false; // Sayfanın reload olmasını engeller
     }
   });
+
+
 
   // Form verilerini topla
   function collectFormData() {
@@ -157,5 +134,17 @@ $(document).ready(function () {
     localStorage.setItem('applications', JSON.stringify(applications));
     console.log("LocalStorage'a kaydedildi:", data);
   }
+
+  $(".checkmark").click(function () {
+    $("#termsCheckbox").trigger("click")
+})
 });
 
+$(function(){
+  $.extend($.validator.messages, {
+            required: "Bu alanın doldurulması zorunludur.",
+            email: "Lütfen geçerli bir e-posta adresi giriniz.",
+            maxlength: $.validator.format("Lütfen en fazla {0} karakter uzunluğunda bir değer giriniz."),
+            minlength: $.validator.format("Lütfen en az {0} karakter uzunluğunda bir değer giriniz."),
+        });
+})
