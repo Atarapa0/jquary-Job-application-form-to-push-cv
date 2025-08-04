402bb134e4aa19f4bdf6cd0a9153a80328111bdb
Atarapa0 <furkaan.er.34@gmail.com>
2025-08-04 15:54:19 +0300

    form sayfasında düzenlemeler yapıldı

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,11 @@
 
 $(document).ready(function () {
-  $('#employmentType').select2({
-    placeholder: "Select employment types",
-    allowClear: true
-  });
-
-  $('#workplaceType').select2({
-    placeholder: "Choose workplace type",
-    allowClear: true
-  });
-
-  $('#jobLocation').select2({
-    placeholder: "Choose location",
+  $('#skills').select2({
+    placeholder: "Select skills",
     allowClear: true
   });
 });
 
-
-/*
-$(document).ready(function () {
-  $("#name").blur(function () {
-    if (this.value.match(/[^a-zA-Z\s]/g)) {
-      this.value = this.value.replace(/[^a-zA-Z\s]/g, '');
-      $("#name-error").text("Sadece harf girişine izin verilir.").show();
-    } else {
-      $("#name-error").hide();
-    }
-  });
-  $("#email").blur(function () {
-    // Email formatını kontrol et
-    var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    if (!emailRegex.test(this.value)) {
-      $("#email-error").text("Geçerli bir e-posta adresi girin.").show();
-    } else {
-      $("#email-error").hide();
-    }
-  });
-
-  $("#phone").blur(function () {
-    var phoneRegex = /^0[0-9]{10}$/; // 10 haneli telefon numarası için basit bir regex
-    if (!phoneRegex.test(this.value)) {
-      $("#phone-error").text("Boşluk bırakmadan geçerli bir telefon numarası girin. Örn: 0555 555 55 55").show();
-    } else {
-      $("#phone-error").hide();
-    }
-  });
-});*/
 
 $(document).ready(function () {
   // Form validation rules
@@ -65,20 +24,46 @@ $(document).ready(function () {
         digits: true,  //pattern: /^[0-9]{10,11}$/,
         minlength: 10,
         maxlength: 13
+      },website: {
+        required: true,
+        url: true
       },
-      workplaceType: {
+       workCompany: {
         required: true
       },
-      employmentType: {
+      workTask: {
         required: true
       },
-      jobLocation: {
+      workDuration: {
         required: true
       },
-
+      workDescription: {
+        required: true
+      },
+      educationName: {
+        required: true
+      },
+      educationLevel: {
+        required: true
+      },
+      educationDate: {
+        required: true
+      },
+      qualificationsName: {
+        required: true
+      },
+      qualificationsCompany: {
+        required: true
+      },
+      qualificationsDate: {
+        required: true
+      },
+      skills: {
+        required: true
+      },
     },
     errorPlacement: function (error, element) {
-      if (element.attr("name") === "workplaceType" || element.attr("name") === "employmentType" || element.attr("name") === "jobLocation") {
+      if (element.attr("name") === "workExperience[]") {
         error.insertAfter(element.next(".select2-container"));
       } else {
         error.insertAfter(element);
@@ -144,7 +129,5 @@ $(function(){
   $.extend($.validator.messages, {
             required: "Bu alanın doldurulması zorunludur.",
             email: "Lütfen geçerli bir e-posta adresi giriniz.",
-            maxlength: $.validator.format("Lütfen en fazla {0} karakter uzunluğunda bir değer giriniz."),
-            minlength: $.validator.format("Lütfen en az {0} karakter uzunluğunda bir değer giriniz."),
         });
 })
